ac1ad8f479ebb18536b72a785d5e744911590f31
Evgenii Samarin <sam.201498@gmail.com>
2025-11-22 13:59:04 +0300

    Initial Commit

---
+++ AdvancedNSTextAttachment/AdvancedNSTextAttachmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvancedNSTextAttachmentApp.swift
+//  AdvancedNSTextAttachment
+//
+//  Created by Евгений Самарин on 22.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct AdvancedNSTextAttachmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
